a223d4649a5b69887d4dd07a84619fe80a6a1430
andrey <andreyparra595@gmail.com>
2020-07-24 17:15:13 -0500

    Creacion proyecto

---
+++ resources/views/paises.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align:center">Lista de paises</h1>
+    <div style="width: 80%; margin-left:10%">
+    <table class="table table-dark table-hover" >
+       <thead>
+         <tr>
+           <th scope="col" class="btn-primary"> Pais</th>
+           <th scope="col" class="btn-success">Capital</th>
+           <th scope="col" class="btn-danger">Moneda</th>
+           <th scope="col" class="btn-info">Población</th>
+           <th scope="col" class="btn-secondary">Ciudades principales</th>
+         </tr>
+       </thead>
+       <tbody>
+          @foreach($paises as $pais => $infopais) 
+              <tr>
+                <td rowspan="3">{{ $pais }}</td>
+                <td rowspan="3">{{ $infopais["capital"]}}</td>
+                <td rowspan="3">{{ $infopais["moneda"]}}</td>
+                <td rowspan="3">{{ $infopais["poblacion"]}}</td>
+                <td class="text-success">{{$infopais["ciudades"][0]    }}</td>
+              </tr>
+              <tr>
+                <td class="text-danger">{{$infopais["ciudades"][1]     }}</td>
+              </tr>
+              <tr>
+                <td class="text-primary">{{$infopais["ciudades"][2]     }}</td>
+              </tr>
+          @endforeach
+       </tbody>
+    </table>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//prueba
+Route::get('hola', function () {
+    echo "hola mundo";
+}); //route tiene un metodo get que vamos a ejecutar
+
+Route::get('arreglo', function () {
+    $estudiantes = ["AN" =>"Ana", 
+                    "MA" => "Maria",
+                    "VA"=> "Valeria", 
+                    "CA" => "Carlos"];
+
+
+    //ciclos foreach: recorrer un arreglo
+    foreach($estudiantes as $indice => $e) {
+        echo "-$e tiene el indice $indice <br> ";
+    }
+});
+
+//Ruta de paises 
+Route::get('paises', function() {
+    $paises = ["Colombia" => ["capital" => "Bogotá",
+                              "moneda" => "peso",
+                              "poblacion" => 503272424,
+                              "ciudades" => ["Medellín", "Cali", "Barranquilla"]             
+                             ], 
+               "Perú" => ["capital" => "Lima",
+                           "moneda" => "sol",
+                           "poblacion" => 33050325,
+                           "ciudades" => ["Cuzco", "Trujillo", "Arequipa"]              
+                         ], 
+               "Ecuador"=> ["capital" => "Quito",
+                            "moneda" => "dolar",
+                            "poblacion" => 17517141,
+                            "ciudades" => ["Guayaquil", "Cuenca", "Manta"]                
+                            ], 
+               "Brasil" => ["capital" => "Brasilia",
+                            "moneda" => "real",
+                            "poblacion" => 21221678,
+                            "ciudades" => ["Rio de Janerio", "Recife", "Bahía"]                 
+                            ]
+             ];
+
+               //enviar los datos de paises a una vista
+               //con la funcion view
+
+               return view('paises')->with("paises", $paises);
+});
